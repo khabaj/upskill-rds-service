fd851a831b6037cb0ab68864163172343b5664b5
khabaj <habajkrystian@gmail.com>
2020-03-18 20:53:51 +0100

    db changes

--- src/main/java/com/pgs/upskill/upskillrdsservice/book/Book.java
+++ src/main/java/com/pgs/upskill/upskillrdsservice/book/Book.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 @Entity
 public class Book {
     @Id
-    private UUID id;
+    private String id;
 
     private String name;
 
@@ -15,15 +15,15 @@ public class Book {
     }
 
     public Book(String name) {
-        this.id = UUID.randomUUID();
+        this.id = UUID.randomUUID().toString();
         this.name = name;
     }
 
-    public UUID getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(UUID id) {
+    public void setId(String id) {
         this.id = id;
     }
 
